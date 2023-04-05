cb764b380d00c664f053fb486a42b4352f37eaf6
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-04-05 11:10:57 +0530

    #1

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int i = 0;
+        if(multiplyByAndToInclusive==0){
+            System.out.print("");
+        }else {
+            while(i<=5){
+                System.out.println(i*multiplyByAndToInclusive);
+                i++;
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int i = 0;
+        while (lastPrinted >= 0) {
+            System.out.println(i);
+            lastPrinted --;
+            i++;
+        }
     }
 }
